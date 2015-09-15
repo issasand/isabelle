a2c15a3343da6175fcd32a038709f49b6d80d132
issasand <isabelle.sandhall@gmail.com>
2015-09-15 09:43:39 +0200

    första commiten

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+	<head>
+		<meta charset=utf-8>
+		<title>Projekt</title>
+		<!--<link rel="stylesheet" href="wp/wp-content/themes/isabelle/style.css" type="text/css">-->
+		<?php get_template_directory_uri()?>/style.css
+		<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<header>
+			<div id="header">
+				<img src="issasandlogosmall.png" id="logo">
+				<h1>Work</h1>
+			</div>
+		</header>
+		<div id="pagecontainer">
+			<div class="box"></div>
+			<div class="box"></div>
+			<div class="box"></div>
+			<div class="box"></div>
+			<div class="box"></div>
+			<div class="box"></div>
+			<div class="box"></div>
+			<div class="box"></div>
+		</div>
+	</body>
+</html>
